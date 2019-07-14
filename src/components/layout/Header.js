a8a4a53662e23813ee34887af55c4e6bd7f34c04
edwinisnow <edwin04@gmail.com>
2019-07-14 08:40:38 +0100

    Links Routing

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return <header style={headerStyle}>Header</header>;
+  return (
+    <header style={headerStyle}>
+      <h1>Todo List</h1>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>{' '}
+      |{' '}
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
+    </header>
+  );
 }
+const linkStyle = {
+  color: '#fff',
+  textDecoratin: 'none'
+};
 
 const headerStyle = {
   backgroundColor: '#333',
