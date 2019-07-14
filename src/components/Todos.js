7de3227c912f4f22e11d862528c1c897f500ebdd
edwinisnow <edwin04@gmail.com>
2019-07-14 09:43:57 +0100

    dynamic todos with json placeholder

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -16,7 +16,9 @@ export class Todos extends Component {
 }
 
 Todos.propTypes = {
-  todos: PropTypes.array.isRequired
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired
 };
 
 export default Todos;
